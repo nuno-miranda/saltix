7259c03dac0c468ec271617d2942651d930a181a
Nuno Miranda <38939683+nuno-miranda@users.noreply.github.com>
2026-05-07 23:03:25 +0100

    Remove .history files

--- .history/preload_20260507162120.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  toggleNotifications: () => ipcRenderer.invoke('toggle-notifications'),
-  getNotificationsEnabled: () => ipcRenderer.invoke('get-notifications-enabled'),
-  newSession: () => ipcRenderer.invoke('new-session'),
-  notifyNewEmail: (from, subject) => ipcRenderer.invoke('notify-new-email', from, subject)
-});
