b72ddd5449eb58d6a73f52b34b616795f9d1326b
Nuno Miranda <nuno-miranda@sapo.pt>
2026-05-07 23:03:25 +0100

    Remove .history files

--- .history/preload_20260507161435.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  toggleNotifications: () => ipcRenderer.invoke('toggle-notifications'),
-  getNotificationsEnabled: () => ipcRenderer.invoke('get-notifications-enabled'),
-  newSession: () => ipcRenderer.invoke('new-session')
-});
